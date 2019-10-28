d081adf1543a0098414d0cb6b286eee0275e8be8
felix <felixcodesg@gmail.com>
2019-10-27 22:38:53 -0400

    working on the slider

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,9 +1,21 @@
-//  Mobile Navigation Toggling
-const menuIcon = document.querySelector('.navigation__icon');
-const hamburgerIcon = document.querySelector('.navigation__hamburger');
-const navList = document.querySelector('.navigation__list');
+window.addEventListener('load', function() {
+  //  Mobile Navigation Toggling
+  const menuIcon = document.querySelector('.navigation__icon');
+  const hamburgerIcon = document.querySelector('.navigation__hamburger');
+  const navList = document.querySelector('.navigation__list');
 
-menuIcon.addEventListener('click', () => {
-  hamburgerIcon.classList.toggle('navigation__hamburger--open');
-  navList.classList.toggle('navigation__list--open');
+  menuIcon.addEventListener('click', () => {
+    hamburgerIcon.classList.toggle('navigation__hamburger--open');
+    navList.classList.toggle('navigation__list--open');
+  });
+
+  const glideConfig = {
+    type: 'carousel',
+    startAt: 2,
+    perView: 2,
+    focusAt: 'center',
+    gap: 20,
+  };
+
+  new Glide('.glide', glideConfig ).mount();
 });
