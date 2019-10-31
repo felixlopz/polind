bd2efcd835640d3d62a459c739e7499fb4b32aec
felix <felixcodesg@gmail.com>
2019-10-31 06:13:41 -0400

    working on search section

---
+++ src/js/places.js
@@ -0,0 +1,30 @@
+const places = [
+	{
+		name: 'poznan',
+		place: 'wielkopolskie',
+		img: 'poznan',
+	},{
+		name: 'warsaw',
+		place: 'Mazowieckie',
+		img: 'warsaw',
+	},{
+		name: 'wroclaw',
+		place: 'Dolnoslaskie',
+		img: 'wroclaw',
+	},{
+		name: 'karpacz',
+		place: 'Dolnoslaskie',
+		img: 'karpacz',
+	},{
+		name: 'Dolina Olczyska',
+		place: 'Malopolskie',
+		img: 'dolina-olczyska',
+	},{
+		name: 'kozy',
+		place: 'Slaskie',
+		img: 'kozy',
+	},
+];
+
+const cardTemplate = document.querySelector ('.card-template');
+console.log (cardTemplate);
