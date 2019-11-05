92845435551434004d22a5a33ef57b6ed3b2b143
felix <felixcodesg@gmail.com>
2019-11-04 22:21:58 -0400

    styling input on search section

--- src/js/places.js
+++ src/js/places.js
@@ -1,3 +1,6 @@
+
+const placesElement = document.querySelector ('.places');
+
 const places = [
 	{
 		name: 'poznan',
@@ -26,5 +29,29 @@ const places = [
 	},
 ];
 
-const cardTemplate = document.querySelector ('.card-template');
-console.log (cardTemplate);
+console.log (placesElement);
+
+function initCards(places){
+	placesElement.innerHTML = `${places.reduce ((html , place) => {
+		return html + getCardTemplate (place);
+	},'')}`
+}
+
+function getCardTemplate({name,place,img}){
+	return `
+		<div class="col-md-4 card">
+      <div class="card__header p-2">
+        <figure class="card__figure">
+          <img class="card__image" src="./assets/${img}.png" alt="nice picture of poland">
+        </figure>
+      </div>
+      <div class="card__body text-center pb-4">
+        <h3 class="card__name">${name}</h3> 
+        <p class="card__place">${place}</p>
+        <a href="#" class="btn card__btn mt-4">view offers</a>
+      </div>
+	  </div>
+	`
+}
+
+initCards(places);
