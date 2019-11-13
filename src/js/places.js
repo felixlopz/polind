e4aa479b64ac37a6eb902c3daafd216ffd52631f
felix <felixcodesg@gmail.com>
2019-11-13 11:13:07 -0400

    finish search section and starting explore section

--- src/js/places.js
+++ src/js/places.js
@@ -29,8 +29,6 @@ const places = [
 	},
 ];
 
-console.log (placesElement);
-
 function initCards(places){
 	placesElement.innerHTML = `${places.reduce ((html , place) => {
 		return html + getCardTemplate (place);
@@ -39,17 +37,19 @@ function initCards(places){
 
 function getCardTemplate({name,place,img}){
 	return `
-		<div class="col-md-4 card">
-      <div class="card__header p-2">
-        <figure class="card__figure">
-          <img class="card__image" src="./assets/${img}.png" alt="nice picture of poland">
-        </figure>
-      </div>
-      <div class="card__body text-center pb-4">
-        <h3 class="card__name">${name}</h3> 
-        <p class="card__place">${place}</p>
-        <a href="#" class="btn card__btn mt-4">view offers</a>
-      </div>
+		<div class="col-md-4 card__column">
+			<div class="card">
+	      <div class="card__header p-2">
+	        <figure class="card__figure">
+	          <img class="card__image" src="./assets/${img}.png" alt="nice picture of poland">
+	        </figure>
+	      </div>
+	      <div class="card__body text-center pb-4">
+	        <h3 class="card__name">${name}</h3> 
+	        <p class="card__place">${place}</p>
+	        <a href="#" class="btn card__btn mt-4">view offers</a>
+	      </div>
+      </div>		
 	  </div>
 	`
 }
